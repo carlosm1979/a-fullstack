d95ad1c88ebef49e8f0fe61c33688b4d2c0db6da
carlosm1979@gmail.com <carlosm1979@gmail.com>
2016-06-22 22:53:28 +0200

    dist/

---
+++ dist/client/app/app.js
@@ -0,0 +1,201 @@
+'use strict';
+
+angular.module('testAppApp', ['testAppApp.constants', 'ngCookies', 'ngResource', 'ngSanitize', 'ngRoute']).config(function ($routeProvider, $locationProvider) {
+  $routeProvider.otherwise({
+    redirectTo: '/'
+  });
+
+  $locationProvider.html5Mode(true);
+});
+//# sourceMappingURL=app.js.map
+
+'use strict';
+
+angular.module('testAppApp.util', []);
+//# sourceMappingURL=util.module.js.map
+
+"use strict";
+
+(function (angular, undefined) {
+	angular.module("testAppApp.constants", []).constant("appConfig", {
+		"userRoles": ["guest", "user", "admin"]
+	});
+})(angular);
+//# sourceMappingURL=app.constant.js.map
+
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+(function () {
+  var MainController = function () {
+    function MainController($http) {
+      _classCallCheck(this, MainController);
+
+      this.$http = $http;
+      this.awesomeThings = [];
+    }
+
+    _createClass(MainController, [{
+      key: '$onInit',
+      value: function $onInit() {
+        var _this = this;
+
+        this.$http.get('/api/things').then(function (response) {
+          _this.awesomeThings = response.data;
+        });
+      }
+    }]);
+
+    return MainController;
+  }();
+
+  angular.module('testAppApp').component('main', {
+    templateUrl: 'app/main/main.html',
+    controller: MainController
+  });
+})();
+//# sourceMappingURL=main.controller.js.map
+
+'use strict';
+
+angular.module('testAppApp').config(function ($routeProvider) {
+  $routeProvider.when('/', {
+    template: '<main></main>'
+  });
+});
+//# sourceMappingURL=main.js.map
+
+'use strict';
+
+angular.module('testAppApp').directive('footer', function () {
+  return {
+    templateUrl: 'components/footer/footer.html',
+    restrict: 'E',
+    link: function link(scope, element) {
+      element.addClass('footer');
+    }
+  };
+});
+//# sourceMappingURL=footer.directive.js.map
+
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var NavbarController = function () {
+  //end-non-standard
+
+  //start-non-standard
+
+  function NavbarController($location) {
+    _classCallCheck(this, NavbarController);
+
+    this.$location = $location;
+  }
+
+  _createClass(NavbarController, [{
+    key: 'isActive',
+    value: function isActive(route) {
+      return route === this.$location.path();
+    }
+  }]);
+
+  return NavbarController;
+}();
+
+angular.module('testAppApp').controller('NavbarController', NavbarController);
+//# sourceMappingURL=navbar.controller.js.map
+
+'use strict';
+
+angular.module('testAppApp').directive('navbar', function () {
+  return {
+    templateUrl: 'components/navbar/navbar.html',
+    restrict: 'E',
+    controller: 'NavbarController',
+    controllerAs: 'nav'
+  };
+});
+//# sourceMappingURL=navbar.directive.js.map
+
+'use strict';
+
+(function () {
+
+  /**
+   * The Util service is for thin, globally reusable, utility functions
+   */
+  function UtilService($window) {
+    var Util = {
+      /**
+       * Return a callback or noop function
+       *
+       * @param  {Function|*} cb - a 'potential' function
+       * @return {Function}
+       */
+
+      safeCb: function safeCb(cb) {
+        return angular.isFunction(cb) ? cb : angular.noop;
+      },
+
+
+      /**
+       * Parse a given url with the use of an anchor element
+       *
+       * @param  {String} url - the url to parse
+       * @return {Object}     - the parsed url, anchor element
+       */
+      urlParse: function urlParse(url) {
+        var a = document.createElement('a');
+        a.href = url;
+
+        // Special treatment for IE, see http://stackoverflow.com/a/13405933 for details
+        if (a.host === '') {
+          a.href = a.href;
+        }
+
+        return a;
+      },
+
+
+      /**
+       * Test whether or not a given url is same origin
+       *
+       * @param  {String}           url       - url to test
+       * @param  {String|String[]}  [origins] - additional origins to test against
+       * @return {Boolean}                    - true if url is same origin
+       */
+      isSameOrigin: function isSameOrigin(url, origins) {
+        url = Util.urlParse(url);
+        origins = origins && [].concat(origins) || [];
+        origins = origins.map(Util.urlParse);
+        origins.push($window.location);
+        origins = origins.filter(function (o) {
+          var hostnameCheck = url.hostname === o.hostname;
+          var protocolCheck = url.protocol === o.protocol;
+          // 2nd part of the special treatment for IE fix (see above):
+          // This part is when using well-known ports 80 or 443 with IE,
+          // when $window.location.port==='' instead of the real port number.
+          // Probably the same cause as this IE bug: https://goo.gl/J9hRta
+          var portCheck = url.port === o.port || o.port === '' && (url.port === '80' || url.port === '443');
+          return hostnameCheck && protocolCheck && portCheck;
+        });
+        return origins.length >= 1;
+      }
+    };
+
+    return Util;
+  }
+
+  angular.module('testAppApp.util').factory('Util', UtilService);
+})();
+//# sourceMappingURL=util.service.js.map
+
+angular.module("testAppApp").run(["$templateCache", function($templateCache) {$templateCache.put("components/footer/footer.html","<div class=\"container\"><p>Angular Fullstack v3.7.6 | <a href=\"https://twitter.com/tyhenkel\">@tyhenkel</a> | <a href=\"https://github.com/DaftMonk/generator-angular-fullstack/issues?state=open\">Issues</a></p></div>");
+$templateCache.put("components/navbar/navbar.html","<div ng-controller=\"NavbarController\" class=\"navbar navbar-default navbar-static-top\"><div class=\"container\"><div class=\"navbar-header\"><button type=\"button\" ng-click=\"nav.isCollapsed = !nav.isCollapsed\" class=\"navbar-toggle\"><span class=\"sr-only\">Toggle navigation</span><span class=\"icon-bar\"></span><span class=\"icon-bar\"></span><span class=\"icon-bar\"></span></button><a href=\"/\" class=\"navbar-brand\">test-app</a></div><div id=\"navbar-main\" uib-collapse=\"nav.isCollapsed\" class=\"navbar-collapse collapse\"><ul class=\"nav navbar-nav\"><li ng-repeat=\"item in nav.menu\" ng-class=\"{active: nav.isActive(item.link)}\"><a ng-href=\"{{item.link}}\">{{item.title}}</a></li></ul></div></div></div>");
+$templateCache.put("app/main/main.html","<header id=\"banner\" class=\"hero-unit\"><div class=\"container\"><h1>No test</h1><p class=\"lead\">Kick-start your next web app with Angular Fullstack</p><img src=\"assets/images/yeoman-462ccecbb1.png\" alt=\"I\'m Yeoman\"/></div></header><div class=\"container\"><div class=\"row\"><div class=\"col-lg-12\"><h1 class=\"page-header\">Features:</h1><ul ng-repeat=\"thing in $ctrl.awesomeThings\" class=\"nav nav-tabs nav-stacked col-md-4 col-lg-4 col-sm-6\"><li><a href=\"#\" uib-tooltip=\"{{thing.info}}\">{{thing.name}}</a></li></ul></div></div></div>");}]);
